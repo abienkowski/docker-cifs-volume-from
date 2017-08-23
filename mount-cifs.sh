38560732e66bc33ca236f092209591e330abf19f
Adrian Bienkowski <adrian.bienkowski@ethoca.com>
2017-08-23 08:00:58 -0400

    Remove credentials file from filesystem.

--- mount-cifs.sh
+++ mount-cifs.sh
@@ -7,11 +7,10 @@ LOCAL_PATH=/mnt/cifs
 CIFS_USERNAME=jenkinsshare
 # -- -- create a credentials file from secret
 SECRET=$(cat /run/secret/$CIFS_USERNAME)
-echo -e "USER=$CIFS_USERNAME\nPASS=$SECRET" > /root/.credentials
 # -- workaround for current Ubuntu host from template
 # -- -- load cifs and fscache modules
 modprobe fscache
 modprobe cifs
 # -- mount cifs filesystem
-MOUNT_OPTS=credentials=/root/.credentials,dom=toronto.ethoca.com,uid=1000,gid=1000,rw
+MOUNT_OPTS=user=$CIFS_USERNAME,pass=$SECRET,dom=toronto.ethoca.com,uid=1000,gid=1000,rw
 mount -t cifs -o $MOUNT_OPTS //$REMOTE_HOST/$REMOTE_PATH $LOCAL_PATH
